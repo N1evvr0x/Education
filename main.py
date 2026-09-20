4cc1cbbd02ab5d2df31a3c6fa3b99cfae40d7ac1
nevvrox <grinbergsasa129@gmail.com>
2026-09-20 20:30:25 +0800

    journal for trading

--- main.py
+++ main.py
@@ -1,16 +1,75 @@
-# This is a sample Python script.
+finance= {}
+def add_transaction(finance: dict):
+    result_balance = 0
+    balance = int(input("balance: "))
+    date = input("date: YY/MM/DD: ")
+    while True:
+        direction = input("direction: buy/sell: ")
+        if direction == "buy" or direction == "sell":
+            break
+        print("Enter only buy or sell: ")
+    while True:
+        category = str(input("category: forex/crypto/index: "))
+        if category == "forex" or category == "crypto" or category == "index":
+            break
+        print("Enter only forex or crypto or index: ")
+    while True:
+        result = str(input("result: s/l or t/p "))
+        if result == "s/l" or result == "t/p":
+            break
+        print("Enter only s/l or t/p: ")
+    amount = int(input("amount: "))
+    transaction_id = int(input("transaction_id: "))
+    if result == "s/l":
+        result_balance = balance - amount
+    elif result == "t/p":
+        result_balance = balance + amount
+    finance[transaction_id] = {
+        "balance": result_balance,
+        "date": date,
+        "result": result,
+        "dir": direction,
+        "category": category,
+        "amount": amount
+    }
+    print("Transaction added!")
+    print()
+    print("Transaction id:", transaction_id)
+    print("Direction:", finance[transaction_id]["dir"])
+    print("Date:", finance[transaction_id]["date"])
+    print("Category:", finance[transaction_id]["category"])
+    print("Result:", finance[transaction_id]["result"])
+    print("Amount:", finance[transaction_id]["amount"])
+    print("Balance:", finance[transaction_id]["balance"])
+    print()
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def delete_transaction(finance: dict):
+    transaction_id = int(input("transaction_id: "))
+    if transaction_id in finance:
+        del finance[transaction_id]
+        print("Transaction deleted!")
+    else:
+        print("Transaction not found!")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def check_transactions(finance: dict) -> int:
+    for v, k in finance.items():
+        print(v, k)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def main():
+    while True:
+        print("1. - add transaction")
+        print("2. - delete transaction")
+        print("3. - check transactions")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            add_transaction(finance)
+        elif choice == "2":
+            delete_transaction(finance)
+        elif choice == "3":
+            check_transactions(finance)
+        else:
+            print("Invalid choice")
+main()
